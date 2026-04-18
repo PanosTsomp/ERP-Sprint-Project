94f7839b76411fcd6484ab202e0209d09d1c6058
Paliga <80478964+Paliga@users.noreply.github.com>
2026-04-18 22:19:16 +0300

    fixed undifined DIR in stock php line 2

--- Integrator/src/stock.php
+++ Integrator/src/stock.php
@@ -1,6 +1,6 @@
 <?php
-require_once DIR . '/../src/auth.php';
-require_once DIR . '/../config.php';
+require_once __DIR__ . '/auth.php';
+require_once __DIR__ . '/../config.php';
 
 function checkStock(string $itemCode, string $warehouse): float {
     $filters = json_encode([
